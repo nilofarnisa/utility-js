70064b2d1582020cc66cc3299b324426895e5171
Nilofar Nisa <nilofarnisa28@gmail.com>
2021-03-03 14:27:03 +0530

    Head function implemented

--- src/add.js
+++ src/add.js
@@ -1,4 +1,9 @@
 const add = (first,second) => {
     return first + second;   
 }
-module.exports = add;
+
+const head = (array) => {
+    return array[0];
+}
+module.exports.add = add;
+module.exports.head = head;
--- src/add.spec.js
+++ src/add.spec.js
@@ -1,13 +1,24 @@
-const add = require('./add');
+const user = require('./add');
+//const head = require('./add');
 
+//var emptyArray  = [];
+//var array = [1,2,3];
 describe('Add', () => {
 
     it('Sum of 0 and 0 is 0', () => {
-        expect(add(0, 0)).toEqual(0);
+        expect(user.add(0, 0)).toEqual(0);
     });
 
     it('Sum of 1 and 0 is 1', () => {
-        expect(add(1, 0)).toEqual(1);
+        expect(user.add(1, 0)).toEqual(1);
     });
+
+    /*it('Head of Empty Array is null', () => {
+        expect(head(emptyArray)).toEqual([]);
+    })*/
+
+    it('Head of Array is 1', () => {
+        expect(user.head([1,2,3])).toEqual(1);
+    })
 
 })
